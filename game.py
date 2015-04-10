6d69dbd08920ea03c408c961e28e93a21e4fcbb6
Busli <kristjansson91@gmail.com>
2015-04-10 00:57:42 +0000

    Color og number game komið og öll virkni orðin 100%. Næst þarf að tengja við merki frá Arduino og frá leik yfir á dælur. Bæta þarf útlit. Ef einhver er að lesa þetta þá ætla ég að lengja þennan texta aðeins bara for teh lulzz \o/

--- game.py
+++ game.py
@@ -1,20 +1,9 @@
 """
 TODO:
-- Láta notanda vita þegar hann er buinn að gera "Cash out"
+- Background image til að gera þetta meira cool?
+- Einhverjar skemmtilegar visual breytingar a GUI-inu
 
-- Þegar maður gerir cash out kemur pop up gluggi sem leyfir þér að velja áfengistegund
-- Mismunandi verð á þeim
-
-- Lækka verðið á drykkjunum
-- Setja quit takka
-- Hvernig á að meðhöndla þegar viðkomandi er búinn að gera cash out
-
-- Tjekka hvort öll þessi message eru ekki rett logic séð
-- Er hægt að birta/taka burt þessi message á betri hátt?
-
-- Taka GUI i gegn
-
-Þriðji leikurinn myndi vera slotmachine leikur, einföld utgáfa
+- Þriðji leikurinn myndi vera slotmachine leikur, einföld utgáfa
 """
 
 from tkinter import *
@@ -23,8 +12,8 @@ from random import randint
 class scoreSystem:
 
     def __init__(self):
-        # Hvernig er best að taka við merkinu fra Arduino?
         self.playerScore = 0
+        # self.userCreditInsert()
 
     def updateScore(self, plusMinus):
         if plusMinus == 1:
@@ -40,61 +29,105 @@ class scoreSystem:
     def bindGameMenuLabel(self, gameMenu):
         self.gameMenu = gameMenu
 
-    def cashout(self):
+    def cashout(self, ammount):
         print('Trying to cash out...')
         if(self.playerScore > 0):
             print('You have successfully cashed out!')
-            self.playerScore = 0
+            self.playerScore -= ammount
             self.gameMenu.updateScore()
         elif(self.playerScore == 0):
             print('Not enough credit to cash out...')
 
+    def userCreditInsert(self):
+        # Hér verður tekið inn merkið frá Arduino og unnið úr því
+        # Hvernig er best að láta RPi hlusta eftir high merki
+        print('I received a signlal!')
+
 #------------------------------------------------------------------
 
+# Glugginn þegar notandi er að kaupa sér áfengi
 class cashOut:
 
-    def __init__(self, master, scoreSystem):
+    def __init__(self, master, scoreSystem, gameMenu):
         self.master = master
         self.scoreSystem = scoreSystem
+        self.gameMenu = gameMenu
         self.createLayout()
 
     def createLayout(self):
         def key(event):
             self.handleKeyboardEvent(event.char)
 
-        self.frame = Frame(self.master, height=20, bd=1)
+        # Aðal ramminn
+        self.frame = Frame(self.master, bd=1, width=200, height=200)
         self.frame.bind("<Key>", key)
         self.frame.focus_set()
-        self.frame.place(relx=.5, rely=.5, anchor="c")
+        self.frame.grid(row=0, padx=700, pady=370)
 
-        Label(self.frame, text="You are cashing out, choose your poison", font=(20)).pack()
+        Label(self.frame, text="You are cashing out, choose your poison", font=(20)).grid(row=0, sticky=W)
 
         btnBadAlcohol = Button(self.frame, text="Bad alcohol: 100", command=lambda id_btn='bad': self.btn_click(id_btn), font=(20))
-        btnBadAlcohol.pack({'fill': 'x'})
+        btnBadAlcohol.grid(row=1, sticky=N+S+E+W, columnspan=2)
         btnGoodAlcohol = Button(self.frame, text="Good alcohol: 200", command=lambda id_btn='good': self.btn_click(id_btn), font=(20))
-        btnGoodAlcohol.pack({'fill': 'x'})
+        btnGoodAlcohol.grid(row=2, sticky=N+S+E+W, columnspan=2)
+
+        Label(self.frame, text="Current credit: ", font=(20)).grid(row=3, column=0, sticky=W)
+        self.credit = StringVar()
+        self.credit.set(self.scoreSystem.getScore())
+        self.currentCreditLabel = Label(self.frame, textvariable=self.credit, font=(20))
+        self.currentCreditLabel.grid(row=3, column=1, sticky=W)
+
+        quitButton = Button(self.frame, text="Quit: Press q", command=lambda id_btn='q': self.btn_click(id_btn), font=(20))
+        quitButton.grid(row=4, sticky=W)
+
+        self.message = StringVar()
+        self.message.set("")
+        self.messageLabel = Label(self.frame, textvariable=self.message, font=(20)).grid(row=5, sticky=W)
 
     def btn_click(self, btn):
-        # Notandi getur átt efni á slæmu áfengi en ekki góðu
-        # gera tjekk svo notanadi geti ekki keypt vitlaust
         if(btn == 'bad'):
-            # kostar 300
-            # Hér er verið að reyna að kaupa slæmt áfengi
-            # Her fer dælan i gang
-            print('Bad alcohol...')
+            if(self.scoreSystem.getScore() >= 100):
+                print('You bought bad alcohol...')
+                self.scoreSystem.cashout(100)
+                # self.pumpAlcohol(1)
+                self.updateScoreLabel()
+                self.updateMessageLabel('buy')
+            else:
+                self.updateMessageLabel('nobuy')
         elif(btn == 'good'):
-            # kostar 500
-            # Hér er verið að reyna að kaupa gott áfengi
-            # Hér fer dælan i gang
-            print('Good alcohol...')
-
+            if(self.scoreSystem.getScore() >= 200):
+                print('You bought good alcohol...')
+                self.scoreSystem.cashout(200)
+                # self.pumpAlcohol(2)
+                self.updateScoreLabel()
+                self.updateMessageLabel('buy')
+            else:
+                self.updateMessageLabel('nobuy')
+        elif(btn == 'q'):
+            self.master.destroy()
+        
     def handleKeyboardEvent(self, key):
         if(key == '1'):
             self.btn_click('bad')
         elif(key == '2'):
             self.btn_click('good')
+        elif(key == 'q' or key == 'Q'):
+            self.btn_click('q')
         else:
             print('Could not find button')
+
+    def updateMessageLabel(self, action):
+        if(action == 'buy'):
+            self.message.set("You have bought a shot!")
+        elif(action == 'nobuy'):
+            self.message.set("Not enough credit")
+
+    def updateScoreLabel(self):
+        self.credit.set(self.scoreSystem.getScore())
+
+    def pumpAlcohol(self, pumpNumber):
+        # Hér verður sent út merki til að dæla réttu áfengi
+        print('Pump number is: ' + str(pumpNumber))
 
 #------------------------------------------------------------------
 
@@ -112,38 +145,38 @@ class guessTheColor:
 
         def key(event):
             self.handleKeyboardEvent(event.char)
-        
-        self.frame = Frame(self.master, height=20, bd=1)
+
+        self.frame = Frame(self.master, bd=1, width=200, height=200)
         self.frame.bind("<Key>", key)
         self.frame.focus_set()
-        self.frame.place(relx=.5, rely=.5, anchor="c")
+        self.frame.grid(row=0, padx=700, pady=370)
 
-        Label(self.frame, text="I want to play a game", font=(20)).pack()
+        Label(self.frame, text="I want to play a game", font=(20)).grid(row=0, sticky=W)
 
         # Colord buttons 
         redButton = Button(self.frame, text="Red: Press 1", command=lambda id_btn='red': self.btn_click(id_btn), background="red", activebackground="#fff", font=(20), fg="white")
-        redButton.pack({'fill': 'x'})
+        redButton.grid(row=1, sticky=N+S+E+W, columnspan=2)
         blueButton = Button(self.frame, text="Blue: Press 2", command=lambda id_btn='blue': self.btn_click(id_btn), background="blue", activebackground="#fff", font=(20), fg="white")
-        blueButton.pack({'fill': 'x'})
+        blueButton.grid(row=2, sticky=N+S+E+W,columnspan=2)
         greenButton = Button(self.frame, text="Green: Press 3", command=lambda id_btn='green': self.btn_click(id_btn), background="green", activebackground="#fff", font=(20), fg="white")
-        greenButton.pack({'fill': 'x'})
+        greenButton.grid(row=3, sticky=N+S+E+W,columnspan=2)
 
         self.makeScoreLabel()
 
     def makeScoreLabel(self):
-        self.stigPlaceholder = Label(self.frame, text="Current credit: ", font=(20)).pack(side=LEFT)
+        self.stigPlaceholder = Label(self.frame, text="Current credit: ", font=(20)).grid(row=4, column=0, sticky=W)
 
         # StringVar() til að geta update-að label dynamicly
         self.credits = StringVar()
         self.credits.set(self.currentCredit)
-        self.stigLabel = Label(self.frame, textvariable=self.credits, font=(20)).pack(side=LEFT)
+        self.stigLabel = Label(self.frame, textvariable=self.credits, font=(20)).grid(row=4, column=1, sticky=W)
 
         quitButton = Button(self.frame, text="Quit: Press q", command=lambda id_btn='quit': self.btn_click(id_btn), font=(20))
-        quitButton.pack()
+        quitButton.grid(row=5, sticky=W)
         
         self.message = StringVar()
         self.message.set("")
-        self.messageLabel = Label(self.frame, textvariable=self.message, font=(20)).pack(side=LEFT)
+        self.messageLabel = Label(self.frame, textvariable=self.message, font=(20)).grid(row=6, sticky=W)
         
     def randomColorGenerator(self):
         rand = randint(0,2)
@@ -227,37 +260,37 @@ class guessTheNumber:
         def key(event):
             self.handleKeyboardEvent(event.char)
         
-        self.frame = Frame(self.master, height=20, bd=1)
+        self.frame = Frame(self.master, bd=1, width=200, height=200)
         self.frame.bind("<Key>", key)
         self.frame.focus_set()
-        self.frame.place(relx=.5, rely=.5, anchor="c")
+        self.frame.grid(row=0, padx=700, pady=370)
 
-        Label(self.frame, text="I want to play a game", font=(20)).pack()
+        Label(self.frame, text="I want to play a game", font=(20)).grid(row=0, sticky=W)
 
         # Colord buttons 
         oneButton = Button(self.frame, text="1: Press 1", command=lambda id_btn='1': self.btn_click(id_btn), font=(20))
-        oneButton.pack({'fill': 'x'})
+        oneButton.grid(row=1, sticky=N+S+E+W, columnspan=2)
         twoButton = Button(self.frame, text="2: Press 2", command=lambda id_btn='2': self.btn_click(id_btn), font=(20))
-        twoButton.pack({'fill': 'x'})
+        twoButton.grid(row=2, sticky=N+S+E+W, columnspan=2)
         threeButton = Button(self.frame, text="3: Press 3", command=lambda id_btn='3': self.btn_click(id_btn), font=(20))
-        threeButton.pack({'fill': 'x'})
+        threeButton.grid(row=3, sticky=N+S+E+W, columnspan=2)
 
         self.makeScoreLabel()
 
     def makeScoreLabel(self):
-        self.stigPlaceholder = Label(self.frame, text="Current credit: ", font=(20)).pack(side=LEFT)
+        self.stigPlaceholder = Label(self.frame, text="Current credit: ", font=(20)).grid(row=4, column=0, sticky=W)
 
         # StringVar() til að geta update-að label dynamicly
         self.credits = StringVar()
         self.credits.set(self.currentCredit)
-        self.stigLabel = Label(self.frame, textvariable=self.credits, font=(20)).pack(side=LEFT)
+        self.stigLabel = Label(self.frame, textvariable=self.credits, font=(20)).grid(row=4, column=1, sticky=W)
 
         quitButton = Button(self.frame, text="Quit: Press q", command=lambda id_btn="q": self.btn_click(id_btn), font=(20))
-        quitButton.pack()
+        quitButton.grid(row=5, sticky=W)
 
         self.message = StringVar()
         self.message.set("")
-        self.messageLabel = Label(self.frame, textvariable=self.message, font=(20)).pack(side=LEFT)
+        self.messageLabel = Label(self.frame, textvariable=self.message, font=(20)).grid(row=6, sticky=W)
 
     def randomNumberGenerator(self):
         rand = randint(1,3)
@@ -334,31 +367,31 @@ class mainMenu:
         def key(event):
             self.handleKeyboardEvent(event.char)
         
-        self.frame = Frame(self.master, bd=1)
+        self.frame = Frame(self.master, bd=1, width=200, height=200)
         self.frame.bind("<Key>", key)
         # Gefa frame focus til að geta notað lyklaborðið
         self.frame.focus_set()
-        self.frame.place(relx=.5, rely=.5, anchor="c")
-        Label(self.frame, text="Choose a game", font=(20)).pack()
+        self.frame.grid(row=0, padx=700, pady=370)
+        
+        Label(self.frame, text="Choose a game", font=(20)).grid(row=0, sticky=W)
 
         btnColorGame = Button(self.frame, text="Play color game: Press 1", command=lambda id_btn="color": self.playGame(id_btn), font=(20))
-        btnColorGame.pack({'fill': 'x'})
-
+        btnColorGame.grid(row=1, sticky=N+S+E+W, columnspan=2)
         btnNumberGame = Button(self.frame, text="Play number game: Press 2", command=lambda id_btn="number": self.playGame(id_btn), font=(20))
-        btnNumberGame.pack({'fill': 'x'})
-
+        btnNumberGame.grid(row=2, sticky=N+S+E+W, columnspan=2)
+        
         btnCashOut = Button(self.frame, text="Cash out: Press c", command=lambda id_btn="c": self.playGame(id_btn), font=(20))
-        btnCashOut.pack({'fill': 'x'})
+        btnCashOut.grid(row=3, sticky=N+S+E+W, columnspan=2)
 
-        Label(self.frame, text="Current credit: ", font=(20)).pack(side=LEFT)
+        Label(self.frame, text="Current credit: ", font=(20)).grid(row=4, column=0, sticky=W)
         self.credit = StringVar()
         self.credit.set(self.score)
         self.currentCreditLabel = Label(self.frame, textvariable=self.credit, font=(20))
-        self.currentCreditLabel.pack(side=LEFT)
+        self.currentCreditLabel.grid(row=4, column=1, sticky=W)
 
         self.message = StringVar()
         self.message.set("")
-        self.messageLabel = Label(self.frame, textvariable=self.message, font=(20)).pack(side=LEFT)
+        self.messageLabel = Label(self.frame, textvariable=self.message, font=(20)).grid(row=5, sticky=W)
 
     def playGame(self, btn):
         if(btn == "color"):
@@ -367,20 +400,23 @@ class mainMenu:
             self.colorWindow = Toplevel()
             self.colorWindow.attributes('-fullscreen', True)
             guessTheColor(self.colorWindow, self.scoreSystem)
+            self.updateMessageLabel('clear')
         elif(btn == "number"):
             print('You have chosen the number game!')
             # open number game
             self.numberWindow = Toplevel()
             self.numberWindow.attributes('-fullscreen', True)
             guessTheNumber(self.numberWindow, self.scoreSystem)
+            self.updateMessageLabel('clear')
         elif(btn == "c"):
-            if(self.scoreSystem.getScore() <= 100):
+            # cash outs
+            if(self.scoreSystem.getScore() < 100):
                 self.updateMessageLabel('write')
                 return
-            elif(self.scoreSystem.getScore() >= 200):
+            elif(self.scoreSystem.getScore() >= 100):
                 self.cashoutWindow = Toplevel()
                 self.cashoutWindow.attributes('-fullscreen', True)
-                cashOut(self.cashoutWindow, self.scoreSystem)
+                cashOut(self.cashoutWindow, self.scoreSystem, self)
         else:
             print('Error, could not open game')
 
@@ -410,8 +446,8 @@ class mainMenu:
 def main():
     root = Tk()
     root.attributes('-fullscreen', True)
-    myScoreSystem = scoreSystem()
     root.title('Game of thrones')
+    myScoreSystem = scoreSystem()
     gameMenu = mainMenu(root, myScoreSystem)
     myScoreSystem.bindGameMenuLabel(gameMenu)
     root.mainloop()
